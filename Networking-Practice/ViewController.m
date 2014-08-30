1bd73ac96908d0675cf3666600bb80dc0459a8e8
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2014-08-30 17:32:21 +0900

    Added async-Images

--- Networking-Practice/ViewController.m
+++ Networking-Practice/ViewController.m
@@ -10,6 +10,7 @@
 #import "AFNetworking.h"
 #import "DDXML.h"
 #import "DDXMLElement+Dictionary.h"
+#import "UIImageView+WebCache.h"
 
 @interface ViewController (){
     NSArray *items;
@@ -63,10 +64,43 @@
     return items.count;
 }
 
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 170;
+}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
+    NSDictionary *item = items[indexPath.row];
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
-    cell.textLabel.text = items[indexPath.row][@"title"];
+    
+    UILabel *label = (UILabel *)[cell viewWithTag:2];
+    label.text = item[@"title"];
+    
+    NSString *imageURLString = @"";
+    NSString *content = item[@"content:encoded"];
+    NSString *pattern = @"(<img.*?src=\\\"http://cdn-ak.b.st-hatena.com/entryimage/)(.*?)(.jpg\\\".*?>)";
+    NSError* error = nil;
+    NSRegularExpression* regex = [NSRegularExpression regularExpressionWithPattern:pattern options:NSRegularExpressionCaseInsensitive error:&error];
+    if (!error){
+        NSArray *matches = [regex matchesInString:content options:0 range:NSMakeRange(0, content.length)];
+        for (NSTextCheckingResult *match in matches){
+            imageURLString = [NSString stringWithFormat:@"http://cdn-ak.b.st-hatena.com/entryimage/%@_l.jpg", [content substringWithRange:[match rangeAtIndex:2]]];
+        }
+    }
+    
+    NSURL *imageURL = [NSURL URLWithString:imageURLString];
+    UIImageView *imageView = (UIImageView *)[cell viewWithTag:1];
+    [imageView sd_setImageWithURL:imageURL
+                        completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, NSURL *imageURL) {
+                            if (cacheType != SDImageCacheTypeMemory) {
+                                [UIView transitionWithView:imageView
+                                                  duration:0.28 options:UIViewAnimationOptionTransitionCrossDissolve | UIViewAnimationOptionCurveLinear | UIViewAnimationOptionAllowUserInteraction
+                                                animations:nil
+                                                completion:nil];
+                            }
+                        }];
+    
     return cell;
  }
 
