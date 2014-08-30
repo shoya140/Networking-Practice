b7482cc42acc5cfbfbf522b15fe2e11d64a09ee5
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2014-08-30 14:57:02 +0900

    Initial Commit

---
+++ Networking-Practice/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Networking-Practice
+//
+//  Created by Shoya Ishimaru on 2014/08/30.
+//  Copyright (c) 2014年 shoya140. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Networking-Practice/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Networking-Practice
+//
+//  Created by Shoya Ishimaru on 2014/08/30.
+//  Copyright (c) 2014年 shoya140. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
